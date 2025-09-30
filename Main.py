b73a620152200a70d3eaa1cd33bd992633f85cf8
Cedric Anderson Jr <CTANDERSON2@Anderson.edu>
2025-09-29 21:54:12 -0400

    Main.py

--- Main.py
+++ Main.py
@@ -13,9 +13,11 @@ Directions:
 """
 
 from datetime import datetime
+from html import escape
 from zoneinfo import ZoneInfo
 
 from flask import Flask, render_template, abort
+from flask import request, redirect
 app = Flask(__name__)
 
 Stock_Items = [
@@ -49,14 +51,51 @@ Stock_Items = [
     },
 ]
 
+def get_item_component(item_text):
+    safe_text = escape(item_text)
+    return f"""
+        <li>
+        <form action="/items" method="post" style="display:inline">
+            <input type="hidden" name="item_del" value="{safe_text}">
+            <input type="submit" value="Mark Complete">
+            <span>{safe_text}</span>
+        </form>
+        </li>
+    """
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def stocks() -> str:
-    return render_template("stocks.html", data=Stock_Items)
+     global Stock_Items
+     if request.method == "POST":
+        ticker = request.form.get("item_add")
+        del_item = request.form.get("item_del")
+
+        if ticker:
+            ticker = ticker.strip()
+            if ticker:
+                new_transaction = {
+                    "date": datetime.now(tz=ZoneInfo("America/Indiana/Indianapolis")),
+                    "price": 0.0,
+                    "ticker": ticker.upper,
+                    "quantity": 0,
+                    "transaction_id": len(Stock_Items),
+                    }
+                Stock_Items.append(new_transaction)
+
+        elif del_item:
+            try:
+                del_item = int(del_item)
+                Stock_Items = [item for item in Stock_Items if item["transaction_id"] != del_item]
+            except ValueError:
+                pass
+        return redirect("/")
+     
+     return render_template("stocks.html", data=Stock_Items)
 
 
-@app.route("/transactions/<int:transaction_id>")
+@app.route("/transactions/<int:transaction_id>", methods=["GET","POST"])
 def transactions(transaction_id) -> str:
+
     if 0 <= transaction_id < len(Stock_Items):
         transaction_details = Stock_Items[transaction_id]
         return render_template("transactions.html", transaction=transaction_details, transaction_id=transaction_id)
